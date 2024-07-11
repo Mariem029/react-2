b166bc1081a9c8ec8cf8bf6cdea943d866b816d9
Mariem Jammeli <162327464+Mariem029@users.noreply.github.com>
2024-07-11 14:05:41 +0200

    first commit

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 36,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Neymar Jr",
+      team: "Paris Saint-Germain",
+      nationality: "Brazil",
+      jerseyNumber: 10,
+      age: 32,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://via.placeholder.com/150"
+    }
+  ];
+  
+  export default players;
